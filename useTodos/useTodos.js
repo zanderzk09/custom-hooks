f97d6e5e9c5ce680a80ed57f7aa04d7a883e583e
Alexander <isaizander09@gmail.com>
2024-06-04 22:20:31 -0600

    useTodos agregado

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import {useReducer, useEffect,} from 'react';
-import {todoReducer} from '../08-useReducer/todoReducer';
+import {todoReducer} from './todoReducer';
 
 const init = () => {
     return JSON.parse(localStorage.getItem('todos')) || [];
